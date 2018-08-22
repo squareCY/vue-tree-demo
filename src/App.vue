0501a8062bf99c59486569ee1667ec4860a53ba8
square <chenyu.wang@chiefclouds.com>
2018-08-22 10:30:54 +0800

    demo build

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <tree :model="treeData"/>
+  </div>
+</template>
+
+<script>
+import tree from './components/tree.vue';
+
+export default {
+  name: 'app',
+  components: {
+    tree
+  },
+  data: function () {
+      return {
+        treeData:{
+            name: 'My Tree',
+            children: [
+                { name: 'hello' },
+                { name: 'wat' },
+                {
+                    name: 'child folder',
+                    children: [
+                        {
+                            name: 'child folder',
+                            children: [
+                                { name: 'hello' },
+                                { name: 'wat' }
+                            ]
+                        },
+                        { name: 'hello' },
+                        { name: 'wat' },
+                        {
+                            name: 'child folder',
+                            children: [
+                                { name: 'hello' },
+                                { name: 'wat' }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+      }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/tree.vue
@@ -0,0 +1,29 @@
+<template id="item-template">
+    <div>
+        <div style="text-align:left">
+            {{model.name}}
+        </div>
+        <!-- 组件中引用当前组件，传入下级数据 -->
+        <div style="margin-left:20px">
+            <tree class="tree" v-for="(model,index) in model.children" :key="index" :model="model"></tree>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'tree',
+  template: '#item-template',
+  props: {
+        model: Object
+  },
+  data: function () {
+      return {
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
